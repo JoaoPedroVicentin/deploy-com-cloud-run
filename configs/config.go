7c72057438c1e4b7a0094c003ce645d73b4207af
JOAO PEDRO LOPES VICENTIN <jplvicen@gmail.com>
2025-09-08 15:59:11 -0300

    feat update load config

--- configs/config.go
+++ configs/config.go
@@ -3,19 +3,38 @@ package configs
 import (
 	"fmt"
 	"os"
+	"path/filepath"
+
+	"github.com/spf13/viper"
 )
 
 type Config struct {
-	ApiKey string
+	ApiKey string `mapstructure:"API_KEY"`
 }
 
 func LoadConfig() (*Config, error) {
-	apiKey := os.Getenv("API_KEY")
-	if apiKey == "" {
+	var config Config
+
+	rootDir, err := os.Getwd()
+	if err == nil {
+		envPath := filepath.Join(rootDir, "../..", ".env")
+		viper.SetConfigFile(envPath)
+		_ = viper.ReadInConfig()
+	}
+
+	viper.AutomaticEnv()
+
+	if err := viper.Unmarshal(&config); err != nil {
+		return nil, err
+	}
+
+	if config.ApiKey == "" {
+		config.ApiKey = os.Getenv("API_KEY")
+	}
+
+	if config.ApiKey == "" {
 		return nil, fmt.Errorf("API_KEY não configurada")
 	}
 
-	return &Config{
-		ApiKey: apiKey,
-	}, nil
+	return &config, nil
 }
